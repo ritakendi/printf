3b4c87b1a144f62c224837dc107a3963920f8072
Adam Keino <keinoadam@gmail.com>
2022-04-15 08:22:00 +0000

    [feature] printer.c

--- printer.c
+++ printer.c
@@ -6,8 +6,44 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+	va_list args;
+	int (*pfunc)(va_list, char *, unsigned int);
+	char *b;
+	unsigned int i = 0, j = 0, length = 0;
 
+	va_start(args, format),	b = malloc(sizeof(char) * 1024);
+	if (!format || !b || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{	print_buf(b, j), free(b), va_end(args);
+				return (-1);
+			}
+			else
+			{	pfunc = get_print_func(format, i + 1);
+				if (pfunc == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(b, format[i], j), length++, i--;
+				}
+				else
+				{
+					length += pfunc(args, b, j);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
+		}
+		else
+			handl_buf(b, format[i], j), length++;
+		for (j = length; j > 1024; j -= 1024)
+			;
+	}
+	print_buf(b, j), free(b), va_end(args);
+	return (length);
+}
