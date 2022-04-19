4634ac484aba003dc188ad8f6c2288f3fb80076a
Adam Keino <keinoadam@gmail.com>
2022-04-19 07:45:52 +0000

    [feature] Tests complete

--- main.h
+++ main.h
@@ -4,9 +4,12 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 int _printf(const char *format, ...);
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+int ev_print_func(const char *s, int index);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+int print_buf(char *buf, unsigned int nbuf);
 
 
 #endif
